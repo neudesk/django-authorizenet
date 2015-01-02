e1de9d177430ffe4a8ed3aee3abf3e8ab2f0653b
neudesk <neudesk@yahoo.com>
2015-01-02 15:35:23 +0800

    Update urls.py

--- authorizenet/urls.py
+++ authorizenet/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls.defaults import patterns, url
+# from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('authorizenet.views',
      url(r'^sim/payment/$', 'sim_payment', name="authnet_sim_payment"),
